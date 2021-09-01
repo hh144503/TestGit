b82c94b668942dc6e41a91f74656909b38609637
hh144503 <1445033808@qq.com>
2021-09-01 22:34:59 +0800

    test2

---
+++ src/Test2.java
@@ -0,0 +1,10 @@
+/**
+ * @author huhai
+ * @version 1.0
+ * @date 2021/9/1 22:33
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
