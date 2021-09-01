b5a7838051aa578cdb287f461cdad452bf103994
hh144503 <1445033808@qq.com>
2021-09-01 22:46:07 +0800

    test3

---
+++ src/Test4.java
@@ -0,0 +1,10 @@
+/**
+ * @author huhai
+ * @version 1.0
+ * @date 2021/9/1 22:44
+ */
+public class Test4 {
+    public static void main(String[] args) {
+        System.out.println("01");
+    }
+}
