cf8cc2d5fe041dca12fa7d0b8dee2930a4918a05
hh144503 <1445033808@qq.com>
2021-09-01 22:16:16 +0800

    test

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * @author huhai
+ * @version 1.0
+ * @date 2021/9/1 22:06
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
