3dd2e3cfd7ccf45cbbcf9babb9476e67d45d2ab2
hh144503 <1445033808@qq.com>
2021-10-12 22:27:40 +0800

    indexof

--- src/Test4.java
+++ src/Test4.java
@@ -6,5 +6,16 @@
 public class Test4 {
     public static void main(String[] args) {
         System.out.println("01");
+        String s = "abcdf" ;
+        if (s.contains("bcd")) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+        if (s.indexOf("bcd") >= 0) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
     }
 }
